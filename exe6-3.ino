4b213ca516c97a68515685bf6919c7fb1d38ac96
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-05-19 20:37:11 -0300

    define variaveis

--- exe6-3.ino
+++ exe6-3.ino
@@ -1,15 +1,18 @@
+//criar variáveis
+#define led 13
+
 void setup(){
-    pinMode(13, OUTPUT);
+    pinMode(led, OUTPUT);
 }
 // o loop repete-se indefinidamente
 void loop(){
     // ligar o led
-    digitalWrite(13, 1);
+    digitalWrite(led, 1);
     // espere por um segundo
     // 1 seg == 1000 milisegundos
     delay(1000);
     // desligar o led
-    digitalWrite(13, 0);
+    digitalWrite(led, 0);
     // espere po um segundo
     delay(1000);
 }
