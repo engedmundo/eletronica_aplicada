2a3d0952119700222ec75c22b594675c9f2f99c1
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-05-19 20:06:10 -0300

    exe 6.1

---
+++ exe6-1.ino
@@ -0,0 +1,13 @@
+// definir as config. iniciais do programa no setup
+void setup(){
+    // definir o pino 13 como saída de informação
+    // pinMode(pino, entrada ou saída)
+    pinMode(13, OUTPUT);
+    // ligar o pino 13
+    // definir o valor de uma saída digital
+    // nas saídas - escrever - Write
+    // digitalWrite(pino, valor digital (0 ou 1))
+    digitalWrite(13, 1);
+}
+
+void loop(){}
