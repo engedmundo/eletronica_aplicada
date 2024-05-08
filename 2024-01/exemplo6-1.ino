4b72a9f6579af87a97cdc1c0bacf97a2f233085f
Edmundo Abreu e Lima <98054216+engedmundo@users.noreply.github.com>
2024-05-08 20:27:28 -0300

    Create exemplo6-1.ino

---
+++ 2024-01/exemplo6-1.ino
@@ -0,0 +1,10 @@
+// comentário
+// inicializar o projeto
+void setup() {
+    // define a porta 13 como saída
+    pinMode(13, OUTPUT);
+    // ligar saida 13
+    digitalWrite(13, 1);
+    // digitalWrite(<porta>, <nível lógico>)
+}
+void loop() {}
