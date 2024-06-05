f445920c9fe9527dff43f62e0a0a8f3288964a47
Edmundo Abreu e Lima <98054216+engedmundo@users.noreply.github.com>
2024-06-05 19:52:13 -0300

    Create exemplo7-1.ino

---
+++ 2024-01/exemplo7-1.ino
@@ -0,0 +1,8 @@
+/* Comentário de múltiplas linhas
+1 - Formulação verbal do problema
+O cilindro de uma prensa para estampagem deve avançar com, no mínimo, uma das seguintes condições de comando satisfeita:
+
+1) Os dois botões manuais devem estar acionados
+2) A grade de proteção deve estar fechada e o pedal acionado
+3) A grade de proteção deve estar fechada e um dos dois comandos manuais deve estar acionado
+*/
